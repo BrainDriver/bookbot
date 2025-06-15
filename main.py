6558f2157eeb00bfb3011408e2fa192d61a40aa0
BrainDriver <raphael_riedel@outlook.de>
2025-06-16 00:15:20 +0200

    added function to count all characters in dict

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from stats import count_words
+from stats import count_words, count_characters
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
     
 def main():
-    count_words(get_book_text("books/frankenstein.txt"))
+    #count_words(get_book_text("books/frankenstein.txt"))
     #print(get_book_text("books/frankenstein.txt"))
+    print(count_characters(get_book_text("books/frankenstein.txt")))
 
 main()
     
--- stats.py
+++ stats.py
@@ -1,2 +1,11 @@
 def count_words(text):
     print(f"{len(text.split())} words found in the document")
+
+def count_characters(text):
+    dict={}
+    for char in text.lower():
+        if char in dict:
+            dict[char]+=1
+        else:
+            dict[char]=1
+    return dict
