0f8227f282dcae1cda66aff1fa7594869c2050c7
BrainDriver <raphael_riedel@outlook.de>
2025-12-25 20:21:11 +0100

    refactored

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
+from stats import count_words 
+
 def get_book_text(filepath):
     with open(filepath) as f:
         file_to_string = f.read()
     return file_to_string
-
-def count_words(string):
-    words = string.split()
-    return len(words)
 
 def main():
     text = get_book_text("./books/frankenstein.txt")
