9f716e17c2d253fb2af4cb7e8ae69f8587535b22
BrainDriver <raphael_riedel@outlook.de>
2025-12-25 20:18:22 +0100

    add relative bookpath & count words

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_to_string = f.read()
+    return file_to_string
+
+def count_words(string):
+    words = string.split()
+    return len(words)
+
+def main():
+    text = get_book_text("./books/frankenstein.txt")
+    print(f"Found {count_words(text)} total words")
+
+main()
