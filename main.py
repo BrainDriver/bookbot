4d45fb47bc6218b755c00017d90afaefcae8e055
BrainDriver <raphael_riedel@outlook.de>
2025-12-26 12:29:39 +0100

    add nice print output

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ def get_book_text(filepath):
 def main():
     text = get_book_text("./books/frankenstein.txt")
     all_characters = count_characters(text)
-    print(f"Found {count_words(text)} total words")
-    print(sort_dict(all_characters))
+
+    print(f"============ BOOKBOT ============\nAnalyzing book found at books/frankenstein.txt...\n----------- Word Count ----------\nFound {count_words(text)} total words\n-------- Character Count --------")
+    for dict in sort_dict(all_characters):
+        if not dict["char"].isalpha():
+            continue
+        print(f"{dict["char"]}: {dict["num"]}")
+    print("============= END ===============")
 main()
